825fb1b4fef7231431f468883a6b9064480e3ab9
Ahmed Ali <ahmedali.96@hotmail.com>
2021-08-07 20:18:37 -0400

    added bot actions

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const instaPuppet = require("./instaPuppet");
 
 (async () => {
   await instaPuppet.init();
+
+  await instaPuppet.login("asfd", "asfd");
 })();
--- instaPuppet.js
+++ instaPuppet.js
@@ -11,10 +11,27 @@ const instaPuppet = {
     });
 
     instaPuppet.page = await instaPuppet.browser.newPage();
+  },
 
+  login: async (username, password) => {
     await instaPuppet.page.goto(URL, {
       waitUntil: "networkidle2",
     });
+
+    await instaPuppet.page.waitForNavigation({ waitUntil: "networkidle2" });
+    await instaPuppet.page.waitFor(1000);
+
+    await instaPuppet.page.type('input[name="username"]', username, {
+      delay: 50,
+    });
+    await instaPuppet.page.type('input[name="password"]', password, {
+      delay: 50,
+    });
+
+    let loginBtn = await instaPuppet.page.$x('//a[contains(text(),"Log in")]');
+    loginBtn = await instaPuppet.page.$x(
+      '//button[contains(text(), "Log in")]'
+    );
   },
 };
 
